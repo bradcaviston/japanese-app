ceaeddee1750d1c9d56ec0d772155fb68b2ff71b
Brad Caviston <bradcaviston@hotmail.com>
2021-03-03 21:06:04 -0500

    Added instructions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,21 @@ function IndexPage() {
             With this app you can study the Japanese Hiragana and Katakana
             writing systems.
           </p>
+          <p>
+            Click begin to go to the selection page. There you can pick which
+            kana you wish to study. Once you have picked your kana you can click
+            “Start Quiz” to start the quiz. The quiz will present a random kana
+            from your selection and give you five seconds to type in the
+            matching romaji for the kana. If you type in the wrong romaji or you
+            run out of time you will get an incorrect mark. You will have three
+            total opportunities to answer the kana correctly before you fail
+            that kana.
+          </p>
+          <p>
+            Once you’ve finished the test you can see your results on the
+            results page where you can hover over any result to see the time it
+            took you to answer the kana correctly and how many attempts it took.
+          </p>
           <ButtonSection>
             <Grow />
             <BigLinkButton to="/quiz/selection">Begin</BigLinkButton>
