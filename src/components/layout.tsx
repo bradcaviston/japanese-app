50268438af8aeb0a5912c17663cffae308802bc1
Brad Caviston <bradcaviston@hotmail.com>
2021-03-06 10:32:04 -0500

    Added page titles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,11 @@
 import React, { FunctionComponent } from "react";
+import { Helmet } from "react-helmet";
 import Navbar from "./navbar";
 
 const Layout: FunctionComponent = ({ children }) => {
   return (
     <>
+      <Helmet title="Japanese App" />
       <Navbar></Navbar>
       <main>{children}</main>
     </>
